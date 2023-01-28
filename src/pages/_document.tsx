7eff7f3f0e3879bd535d9001b57d48f398afddf9
Jakub Pietrzak <jpietrzi@gmail.com>
2023-01-28 14:38:52 +0100

    next app init

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  // explore this as well
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
